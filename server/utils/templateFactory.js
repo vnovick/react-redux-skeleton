df6283bc72d1c977cdb13706149e3d6f9797750c
Vladimir Novick <vnovick@gmail.com>
2016-04-20 11:28:55 +0300

    deleting legacy code

--- server/utils/templateFactory.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by vladimirn on 11/27/15.
- */
-import fs from 'fs';
-import _ from 'lodash';
-
-export default (templateName = '') => {
-    return _.template(fs.readFileSync(`views/pages/${templateName}`, {'encoding': 'utf8'}));
-}
